d04d216a0a3918678cae8e91a3523a337e02067e
iosub <1330995+iosub@users.noreply.github.com>
2024-08-03 08:17:06 +0200

    rr

---
+++ update.ps1
@@ -0,0 +1,11 @@
+pip install -U langchain
+pip install -U langchain.core  
+pip install -U langchain.community
+
+pip install -U langchain.experimental
+pip install -U langchain.openai
+pip install -U langchain.ollama
+pip install -U langgraph
+pip install beautifulsoup4
+pip install scikit-learn
+pip install langchainhub
